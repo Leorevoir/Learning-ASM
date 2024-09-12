1847370f9eaeec56d02dd4624f7bab935996503f
Leorevoir <leoquinzler@epitech.eu>
2024-09-12 17:00:29 +0200

    fix: remove strcat function signature

--- _strcat.asm
+++ _strcat.asm
@@ -5,27 +5,6 @@
 ;; _strcat
 ;;
 
-
-; char *my_strcat(char *dest, char const *src)
-; {
-;     int a = 0;
-;     int b = 0;
-
-;     if (dest == 0 || src == 0) {
-;         return (0);
-;     }
-;     while (dest[a] != '\0') {
-;         a++;
-;     }
-;     while (src[b] != '\0') {
-;         dest[a] = src[b];
-;         a++;
-;         b++;
-;     }
-;     dest[a] = '\0';
-;     return (dest);
-; }
-
 section .text
     global _strcat
 
