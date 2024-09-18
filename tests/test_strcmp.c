c71cad88e573a002d0731a4d8f719e7a81b1a7ca
Leorevoir <leo.quinzler@epitech.eu>
2024-09-18 21:22:18 +0200

    upadte: strcmp test with harder level

--- tests/test_strcmp.c
+++ tests/test_strcmp.c
@@ -10,21 +10,57 @@
 
 extern int _strcmp(const char *__s1, const char *__s2);
 
+static _Bool easy_test(void)
+{
+    const char *str1 = "Hello";
+    const char *str2 = "Hello";
+    int result = _strcmp(str1, str2);
+
+    if (result != 0) {
+        fprintf(stderr, "Error:\n\t_strcmp(\"%s\",\
+\"%s\") returned %d but expected 0\n", str1, str2, result);
+        return 0;
+    }
+    return 1;
+}
+
+static _Bool medium_test(void)
+{
+    const char *str1 = "Hello";
+    const char *str2 = "Hellp";
+    int result = _strcmp(str1, str2);
+
+    if (result >= 0) {
+        fprintf(stderr, "Error:\n\t_strcmp(\"%s\",\
+\"%s\") returned %d but expected a negative value\n", str1, str2, result);
+        return 0;
+    }
+    return 1;
+}
+
+static _Bool hard_test(void)
+{
+    const char *str1 = "Hello World!";
+    const char *str2 = "Hello";
+    int result = _strcmp(str1, str2);
+
+    if (result <= 0) {
+        fprintf(stderr, "Error:\n\t_strcmp(\"%s\",\
+\"%s\") returned %d but expected a positive value\n", str1, str2, result);
+        return 0;
+    }
+    return 1;
+}
+
 int main(void)
 {
-    const char *first = "Hello, world!";
-    const char *second = "Helld, world!";
-    const int test1 = _strcmp(first, first);
-    const int test2 = _strcmp(first, second);
-
-    if (strcmp(first, first) != test1) {
-        fprintf(stderr, "Error:\n\t_strcmp did not return the right value\
- if both strings are the equal.\n\texpected:\t0\n\tgot\t%d\n", test1);
+    if (!easy_test()) {
         return 84;
     }
-    if (strcmp(first, second) != test2) {
-        fprintf(stderr, "Error:\n\t_strcmp did not return the right value\
- if both strings are differents.\n\texpected:\t11\n\tgot\t%d\n", test2);
+    if (!medium_test()) {
+        return 84;
+    }
+    if (!hard_test()) {
         return 84;
     }
     return 0;
