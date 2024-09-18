53f141634d1af6bee56af0a21f62de818da1cd8f
Leorevoir <leo.quinzler@epitech.eu>
2024-09-18 21:25:50 +0200

    update: test_strcat with harder case

--- tests/test_strcat.c
+++ tests/test_strcat.c
@@ -10,18 +10,77 @@
 
 extern char *_strcat(char *__restrict__ dest, const char *__restrict__ src);
 
+static _Bool easy_test(void)
+{
+    char dest[64];
+    char test[64];
+    
+    strcpy(dest, "Hello, ");
+    strcpy(test, "Hello, ");
+    _strcat(dest, "World!");
+    strcat(test, "World!");
+    if (strcmp(dest, test) != 0) {
+        fprintf(stderr, "Error:\n\t_strcat(\"Hello, \",\
+\"World!\") returned %s but expected %s\n",
+            dest, test);
+        return 0;
+    }
+    return 1;
+}
+
+static _Bool medium_test(void)
+{
+    char dest[64];
+    char test[64];
+    
+    strcpy(dest, "Hello, ");
+    strcpy(test, "Hello, ");
+    _strcat(dest, "");
+    strcat(test, "");
+    if (strcmp(dest, test) != 0) {
+        fprintf(stderr, "Error:\n\t_strcat(\"Hello, \",\
+\"\") returned %s but expected %s\n", dest, test);
+        return 0;
+    }
+    strcpy(dest, "");
+    strcpy(test, "");
+    _strcat(dest, "Hello");
+    strcat(test, "Hello");
+    if (strcmp(dest, test) != 0) {
+        fprintf(stderr, "Error:\n\t_strcat(\"\",\
+\"Hello\") returned %s but expected %s\n", dest, test);
+        return 0;
+    }
+    return 1;
+}
+
+static _Bool hard_test(void)
+{
+    char dest[64];
+    char test[64];
+    
+    strcpy(dest, "1234567890");
+    strcpy(test, "1234567890");
+    _strcat(dest, "abcdefghij");
+    strcat(test, "abcdefghij");
+    if (strcmp(dest, test) != 0) {
+        fprintf(stderr, "Error:\n\t_strcat(\"1234567890\",\
+\"abcdefghij\") returned %s but expected %s\n", dest, test);
+        return 0;
+    }
+    return 1;
+}
 
 int main(void)
 {
-    char __dest[32] = "Hello, ";
-    char __test[32] = "Hello, ";
-    char __src[] = "World!";
-
-    _strcat(__dest, __src);
-    strcat(__test, __src);
-    if (strcmp(__dest, __test) != 0) {
-        fprintf(stderr, "Error:\n\t_strcat returns %s but expected %s\n",
-            __dest, __test);
+    if (!easy_test()) {
+        return 84;
+    }
+    if (!medium_test()) {
+        return 84;
+    }
+    if (!hard_test()) {
+        return 84;
     }
     return 0;
 }
